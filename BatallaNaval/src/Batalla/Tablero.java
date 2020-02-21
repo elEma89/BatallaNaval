2d33eedcff0ddf3c33316726e909575e19fe5cfb
elEma89 <61033552+elEma89@users.noreply.github.com>
2020-02-21 19:04:31 -0300

    Update Tablero.java

--- BatallaNaval/src/Batalla/Tablero.java
+++ BatallaNaval/src/Batalla/Tablero.java
@@ -14,8 +14,8 @@ public class Tablero {
 		//arreglo1 para el tablero de ataques, dos
 		//arreglo2 para el tablero de barcos, dos
 		//variables lanchas igual a 5 en un comienzo, lanchas usuario 1 y lanchas usuario 2	
-			//función poner lanchas, poner lanchas 2
-		//crear funcion para realizar ataques, compara con llamada a ubicación de lancha
+			//funciÃ³n poner lanchas, poner lanchas 2
+		//crear funcion para realizar ataques, compara con llamada a ubicaciÃ³n de lancha
 			//crear funcion para el resultado
 		//define hundido o agua, contador hundido, si hundido igual a 5 gana
 			//funcion para mostrar el tablero
@@ -33,6 +33,6 @@ public class Tablero {
 		 
 		
 				
-	}
+	}hola
 	}
 
