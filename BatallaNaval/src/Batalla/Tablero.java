d9530a95af297603e94bcc03a402c74f6a9dc982
Admin <Admin@LAPTOP-COUPT3PE>
2020-02-21 18:47:05 -0300

    Batalla Naval para todes

---
+++ BatallaNaval/src/Batalla/Tablero.java
@@ -0,0 +1,38 @@
+package Batalla;
+import java.util.*;
+
+
+
+public class Tablero {
+	
+	
+	public int poneLancha () {
+		int i=0;
+	return i;
+	}
+		
+		//arreglo1 para el tablero de ataques, dos
+		//arreglo2 para el tablero de barcos, dos
+		//variables lanchas igual a 5 en un comienzo, lanchas usuario 1 y lanchas usuario 2	
+			//función poner lanchas, poner lanchas 2
+		//crear funcion para realizar ataques, compara con llamada a ubicación de lancha
+			//crear funcion para el resultado
+		//define hundido o agua, contador hundido, si hundido igual a 5 gana
+			//funcion para mostrar el tablero
+			//funcion o contador que definira cuantas lanchas quedan por usuario
+			
+
+	
+	public Tablero() {
+		
+		//arreglo
+		String [][] tableroJuego;
+		
+		int lancha = 5;  
+		
+		 
+		
+				
+	}
+	}
+
---
+++ BatallaNaval/src/Batalla/Usuario.java
@@ -0,0 +1,21 @@
+package Batalla;
+
+public class Usuario {
+
+	public static void main(String[] args) {
+
+		//Crear la matriz {{~,~,~,~,~,~,~},
+		//					{~,~,~,~,~,~,~}...
+		
+		//Mostrar la matriz y solicitar el ingreso de las 5 lanchas
+		
+		//Guardar lanchas en matriz de jugador 1 o 2
+		
+		//Se repite para el otro jugador
+		
+		//Inicio de juego
+		
+		
+	}
+
+}
