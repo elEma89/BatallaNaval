216f81d4a76551d8ae6044e283538faacfa8dc7c
Isaac Pizarro <Isaac Pizarro@DESKTOP-PE8UA4P>
2020-02-22 00:50:02 -0300

    se carga lo visto en la clase

--- BatallaNaval/src/Batalla/Tablero.java
+++ BatallaNaval/src/Batalla/Tablero.java
@@ -1,14 +1,81 @@
 package Batalla;
+
 import java.util.*;
 
 
 
 public class Tablero {
 	
+String [][] tab ;
 	
-	public int poneLancha () {
-		int i=0;
-	return i;
+	public Tablero(String [][] tab) {
+		
+		this.tab=tab;
+		
+				
+	}
+	//imprime el tablero en pantalla, se puede agregar que tablero mostrar
+	public  void muestra_tabla() {
+		
+		for (int i=0;i<tab.length;i++) {
+			for (int j=0;j<tab.length;j++) {
+				System.out.print("["+tab[i][j]+"]");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
+	//modifica la cordenada ingresada por el usuario
+	public void modifica_tabla(int a, int b) {
+		
+		if (this.tab[a][b].equals("~")) {
+			
+			this.tab[a][b]="N";
+			
+		}else{
+			System.out.println("puebe en otra coordenada");
+		}
+		
+		muestra_tabla();
+		
+	}
+	
+	//asigna un numero segun la letra de la cordenada
+	public int numero_letra(String letra) {
+		int cord1=0;
+		
+		switch (letra) {
+		case "a":
+			cord1 = 1;
+			letra="";
+			break;
+		case "b":
+			cord1 = 2;
+			letra="";
+			break;
+		case "c":
+			cord1 = 3;
+			letra="";
+			break;
+		case "d":
+			cord1 = 4;
+			letra="";
+			break;
+		case "e":
+			cord1 = 5;
+			letra="";
+			break;
+		}//fin switch
+		
+		return cord1;
+	}
+	
+	public void limpiar_pantalla() {
+		for(int i=0;i<80;i++) {
+			System.out.println();
+		}
 	}
 		
 		//arreglo1 para el tablero de ataques, dos
@@ -23,16 +90,6 @@ public class Tablero {
 			
 
 	
-	public Tablero() {
-		
-		//arreglo
-		String [][] tableroJuego;
-		
-		int lancha = 5;  
-		
-		 
-		
-				
-	}
+	
 	}
 
--- BatallaNaval/src/Batalla/Usuario.java
+++ BatallaNaval/src/Batalla/Usuario.java
@@ -1,8 +1,54 @@
 package Batalla;
 
+import java.util.*;
+
+
 public class Usuario {
 
 	public static void main(String[] args) {
+		
+		int cord1 = 0;
+		int cord2 = 0;
+		String n_jug;
+		String letra;
+		String[][] tab = { { " ", "1", "2", "3", "4", "5" }, 
+							{ "A", "~", "~", "~", "~", "~" },
+							{ "B", "~", "~", "~", "~", "~" }, 
+							{ "C", "~", "~", "~", "~", "~" }, 
+							{ "D", "~", "~", "~", "~", "~" },
+							{ "E", "~", "~", "~", "~", "~" }, };
+
+		System.out.println("*** BATALLA NAVAL ***");
+		System.out.println();
+		Scanner leo = new Scanner(System.in);
+		Tablero tab1 = new Tablero(tab);
+
+		System.out.println();
+		System.out.println("*****************************");
+		System.out.println("Ingrese su nombre de Jugador:");
+		n_jug = leo.nextLine();
+		// System.out.println();
+		System.out.println("*****************************");
+		System.out.println("Ingrese coordenadas para ingresa su nave");
+		System.out.println("por letra y numero de la tabla");
+
+		tab1.muestra_tabla();
+		System.out.println("*****************************");
+		
+		//controla el ingreso de 5 naves
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("Ingrese letra de coordenada "+(i+1));
+			cord1=tab1.numero_letra(leo.next());
+			
+			System.out.println("Ingrese numero de coordenada "+(i+1));
+			cord2 = leo.nextInt();
+
+			System.out.println();
+			
+			tab1.modifica_tabla(cord1, cord2);
+
+		}//fin for
 
 		//Crear la matriz {{~,~,~,~,~,~,~},
 		//					{~,~,~,~,~,~,~}...
